bd53d3ceee5b25bdc5bedf62db7deeaad5c91a14
Francis Lara <lara.francis1@gmail.com>
2017-10-23 14:37:29 -0700

    Complete first routes exercise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+    # redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user
+      @user.delete
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email)
+  end
+
+end
